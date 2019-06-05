734ce37aa3abf0d9308028a6462ff11b73ba0e14
Kosui Iwasa <contact@ebiebievidence.com>
2019-06-05 13:51:10 +0900

    Implement UserRepo

--- app/repository/user.go
+++ app/repository/user.go
@@ -1,8 +1,10 @@
 package repository
 
 import (
+	"crowns/app/domain/command"
 	"crowns/app/domain/query"
 	"database/sql"
+	"errors"
 
 	"github.com/lib/pq"
 )
@@ -11,14 +13,106 @@ type UserRepository struct {
 	db *sql.DB
 }
 
-func (repo *UserRepository) FindByName(name string) (query.UserQueryModel, error) {
-	var userId *sql.NullInt64
-	var email *sql.NullString
-	var displayName *sql.NullString
-	var token *sql.NullString
-	var createdAt *pq.NullTime
-	
-	err := repo
-		.db.QueryRow("SELECT user_id, email, display_name, token, created_at FROM users WHERE users.display_name = $1", name)
-		.Scan(userId, email, displayName, token, createdAt)
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{
+		db,
+	}
+}
+
+func (repo *UserRepository) FindByName(name string) (userQueryModel *query.UserQueryModel, err error) {
+	userID := &sql.NullInt64{}
+	email := &sql.NullString{}
+	displayName := &sql.NullString{}
+	token := &sql.NullString{}
+	createdAt := &pq.NullTime{}
+
+	err = repo.db.QueryRow("SELECT user_id, email, display_name, token, created_at FROM users WHERE users.display_name = $1", name).Scan(userID, email, displayName, token, createdAt)
+	if err != nil {
+		return nil, err
+	}
+
+	if userID == nil || email == nil || displayName == nil || token == nil || createdAt == nil {
+		return nil, errors.New("Cannot allow null object")
+	}
+
+	userQueryModel = query.NewUserQueryModel(*userID, *email, *displayName, *token, *createdAt)
+
+	return userQueryModel, nil
+}
+
+func (repo *UserRepository) FindByNameCaseInsensitive(name string) (userQueryModel *query.UserQueryModel, err error) {
+	userID := &sql.NullInt64{}
+	email := &sql.NullString{}
+	displayName := &sql.NullString{}
+	token := &sql.NullString{}
+	createdAt := &pq.NullTime{}
+
+	err = repo.db.QueryRow("SELECT user_id, email, display_name, token, created_at FROM users WHERE LOWER(users.display_name) = LOWER($1)", name).Scan(userID, email, displayName, token, createdAt)
+	if err != nil {
+		return nil, err
+	}
+
+	if userID == nil || email == nil || displayName == nil || token == nil || createdAt == nil {
+		return nil, errors.New("Cannot allow null object")
+	}
+
+	userQueryModel = query.NewUserQueryModel(*userID, *email, *displayName, *token, *createdAt)
+
+	return userQueryModel, nil
+}
+
+func (repo *UserRepository) FindByToken(token string) (userQueryModel *query.UserQueryModel, err error) {
+	userID := &sql.NullInt64{}
+	email := &sql.NullString{}
+	displayName := &sql.NullString{}
+	tokenToFind := &sql.NullString{}
+	createdAt := &pq.NullTime{}
+
+	err = repo.db.QueryRow("SELECT user_id, email, display_name, token, created_at FROM users WHERE users.token = $1", token).Scan(userID, email, displayName, tokenToFind, createdAt)
+	if err != nil {
+		return nil, err
+	}
+
+	if userID == nil || email == nil || displayName == nil || tokenToFind == nil || createdAt == nil {
+		return nil, errors.New("Cannot allow null object")
+	}
+
+	userQueryModel = query.NewUserQueryModel(*userID, *email, *displayName, *tokenToFind, *createdAt)
+
+	return userQueryModel, nil
+}
+
+func (repo *UserRepository) FindByID(userID int64) (userQueryModel *query.UserQueryModel, err error) {
+	userIDToFind := &sql.NullInt64{}
+	email := &sql.NullString{}
+	displayName := &sql.NullString{}
+	tokenToFind := &sql.NullString{}
+	createdAt := &pq.NullTime{}
+
+	err = repo.db.QueryRow("SELECT user_id, email, display_name, token, created_at FROM users WHERE users.user_id = $1", userID).Scan(userIDToFind, email, displayName, tokenToFind, createdAt)
+	if err != nil {
+		return nil, err
+	}
+
+	if userIDToFind == nil || email == nil || displayName == nil || tokenToFind == nil || createdAt == nil {
+		return nil, errors.New("Cannot allow null object")
+	}
+
+	userQueryModel = query.NewUserQueryModel(*userIDToFind, *email, *displayName, *tokenToFind, *createdAt)
+
+	return userQueryModel, nil
+}
+
+func (repo *UserRepository) Save(userCommandModel *command.UserCommandModel) (userID *int64, err error) {
+	userIDToFind := &sql.NullInt64{}
+	err = repo.db.QueryRow("INSERT INTO users (email, display_name, token) VALUES ($1, $1, $2) RETURNING user_id", userCommandModel.Name, userCommandModel.Token).Scan(userIDToFind)
+	if err != nil {
+		return nil, err
+	}
+
+	if !userIDToFind.Valid {
+		return nil, errors.New("failed to insert")
+	}
+
+	return &userIDToFind.Int64, nil
 }
