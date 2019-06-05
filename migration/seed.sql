e1efa04ff949c8bafe2bdc21fe37d47b65294f20
Kosui Iwasa <contact@ebiebievidence.com>
2019-06-05 16:56:42 +0900

    Add seed

---
+++ migration/seed.sql
@@ -0,0 +1,27 @@
+INSERT INTO
+cards (
+    is_child,
+    scenario_id,
+    card_text,
+    card_image
+) VALUES (
+    False,
+    NULL,
+    '1',
+    '1'
+), (
+    False,
+    NULL,
+    '2',
+    '2'
+), (
+    False,
+    NULL,
+    '3',
+    '3'
+), (
+    False,
+    NULL,
+    '4',
+    '4'
+)
