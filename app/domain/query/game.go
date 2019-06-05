815dd0d96b7aaf3efe95846587982ee861e30377
Kosui Iwasa <contact@ebiebievidence.com>
2019-06-05 13:49:50 +0900

    Remove unused

--- app/domain/query/game.go
+++ app/domain/query/game.go
@@ -8,28 +8,22 @@ import (
 )
 
 type GameQueryModel struct {
-	GameID       int64
-	UserID       int64
-	Score        int64
-	EmperorImina string
-	EmperorGengo string
-	CreatedAt    time.Time
+	GameID    int64
+	UserID    int64
+	Score     int64
+	CreatedAt time.Time
 }
 
 func NewGameQueryModel(
 	gameID sql.NullInt64,
 	userID sql.NullInt64,
 	score sql.NullInt64,
-	emperorImina sql.NullString,
-	emperorGengo sql.NullString,
 	createdAt pq.NullTime,
 ) *GameQueryModel {
 	return &GameQueryModel{
-		GameID:       gameID.Int64,
-		UserID:       userID.Int64,
-		Score:        score.Int64,
-		EmperorImina: emperorImina.String,
-		EmperorGengo: emperorGengo.String,
-		CreatedAt:    createdAt.Time,
+		GameID:    gameID.Int64,
+		UserID:    userID.Int64,
+		Score:     score.Int64,
+		CreatedAt: createdAt.Time,
 	}
 }
