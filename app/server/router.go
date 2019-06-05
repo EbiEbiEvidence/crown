b00a2abb13b6ec5c1dae7d426e7ee2324d94ec20
Kosui Iwasa <contact@ebiebievidence.com>
2019-06-05 13:51:19 +0900

    Inject

--- app/server/router.go
+++ app/server/router.go
@@ -2,9 +2,18 @@ package server
 
 import (
 	"github.com/julienschmidt/httprouter"
+
+	"crowns/app/handler"
+	"crowns/app/repository"
+	"crowns/app/usecase"
 )
 
 func (s *SimpleServer) Router() *httprouter.Router {
 	h := httprouter.New()
+	userRepository := repository.NewUserRepository(s.db)
+	userUseCase := usecase.NewUserUseCase(userRepository)
+	userHandler := handler.NewUserHandler(userUseCase)
+	h.POST("/user/get", userHandler.Get)
+	h.POST("/user/create", userHandler.Create)
 	return h
 }
