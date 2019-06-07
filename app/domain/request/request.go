0af57d1500064a88a08d9ffc1f8800696e1e0935
Kosui Iwasa <contact@ebiebievidence.com>
2019-06-07 16:26:59 +0900

    BUGFIX: invalid tag

--- app/domain/request/request.go
+++ app/domain/request/request.go
@@ -1,8 +1,8 @@
 package request
 
 type GetUser struct {
-	Name  string `json:"name",omitempty`
-	Token string `json:"token",omitempty`
+	Name  string `json:"name,omitempty"`
+	Token string `json:"token,omitempty"`
 }
 
 type CreateUser struct {
@@ -10,20 +10,20 @@ type CreateUser struct {
 }
 
 type StartGame struct {
-	Token string `json:"token",omitempty`
+	Token string `json:"token,omitempty"`
 }
 
 type GetHighScores struct {
-	Token string `json:"token",omitempty`
+	Token string `json:"token,omitempty"`
 }
 
 type SubmitHighScores struct {
-	Token          string `json:"token",omitempty`
-	Start          int64  `json:"start",omitempty`
-	Age            int64  `json:"age",omitempty`
-	ChurchScore    int64  `json:"church_score",omitempty`
-	CommersScore   int64  `json:"commers_score",omitempty`
-	MerchantsScore int64  `json:"merchants_score",omitempty`
-	MilitaryScore  int64  `json:"military_score",omitempty`
-	BonusScore     int64  `json:"bonus_score",omitempty`
+	Token          string `json:"token,omitempty"`
+	Start          int64  `json:"start,omitempty"`
+	Age            int64  `json:"age,omitempty"`
+	ChurchScore    int64  `json:"church_score,omitempty"`
+	CommersScore   int64  `json:"commers_score,omitempty"`
+	MerchantsScore int64  `json:"merchants_score,omitempty"`
+	MilitaryScore  int64  `json:"military_score,omitempty"`
+	BonusScore     int64  `json:"bonus_score,omitempty"`
 }
--- app/domain/response/response.go
+++ app/domain/response/response.go
@@ -10,18 +10,18 @@ type User struct {
 type HighScore struct {
 	HighScoreID int64          `json:"highScoreID"`
 	UserID      int64          `json:"userID"`
-	Start       int64          `json:"start",omitempty`
-	Age         int64          `json:"age",omitempty`
+	Start       int64          `json:"start,omitempty"`
+	Age         int64          `json:"age,omitempty"`
 	CreatedAt   time.Time      `json:"created_at"`
 	Scores      HighScoreScore `json:"scores"`
 }
 
 type HighScoreScore struct {
-	ChurchScore    int64 `json:"church_score",omitempty`
-	CommersScore   int64 `json:"commers_score",omitempty`
-	MerchantsScore int64 `json:"merchants_score",omitempty`
-	MilitaryScore  int64 `json:"military_score",omitempty`
-	BonusScore     int64 `json:"bonus",omitempty`
+	ChurchScore    int64 `json:"church_score,omitempty"`
+	CommersScore   int64 `json:"commers_score,omitempty"`
+	MerchantsScore int64 `json:"merchants_score,omitempty"`
+	MilitaryScore  int64 `json:"military_score,omitempty"`
+	BonusScore     int64 `json:"bonus,omitempty"`
 }
 
 type Game struct {
